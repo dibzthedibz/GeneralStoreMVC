5ed86f772aa2a710cbcf3e55abb3c5d4ac72884d
Ryan <Dibzthedibz@gmail.com>
2021-06-22 07:00:53 -0700

    Product Create Finished, tested, proven. Began Creation of Delete methodology and views.

--- GeneralStore.MVC/Controllers/ProductController.cs
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -15,7 +15,35 @@ namespace GeneralStore.MVC.Controllers
         {
             return View(_db.Products.ToList());
         }
-
-        
+        //Get: Product
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //Post: Product
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+        //Get: Delete Product/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
     }
 }

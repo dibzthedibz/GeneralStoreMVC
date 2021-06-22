3a8f9971f72173e83b958105580d79de2433f302
Ryan <Dibzthedibz@gmail.com>
2021-06-22 10:09:41 -0700

    Transaction class added, populated, and migrated.

---
+++ GeneralStore.MVC/Migrations/202106221708595_TransactionAdded.cs
@@ -0,0 +1,37 @@
+namespace GeneralStore.MVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TransactionAdded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Transactions",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ProductId = c.Int(nullable: false),
+                        CustomerId = c.Int(nullable: false),
+                        PQuan = c.Int(nullable: false),
+                        DateOfTransaction = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Customers", t => t.CustomerId, cascadeDelete: true)
+                .ForeignKey("dbo.Products", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.ProductId)
+                .Index(t => t.CustomerId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Transactions", "ProductId", "dbo.Products");
+            DropForeignKey("dbo.Transactions", "CustomerId", "dbo.Customers");
+            DropIndex("dbo.Transactions", new[] { "CustomerId" });
+            DropIndex("dbo.Transactions", new[] { "ProductId" });
+            DropTable("dbo.Transactions");
+        }
+    }
+}
